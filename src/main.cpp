11af57961775d55232641e9873d5873c87b93479
Isaac Trimble-Pederson <wtpisaac@gmail.com>
2026-04-17 00:15:12 -0600

    doc: copyrights

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,26 @@
+/*
+    implementing simple soccer using ecs and raylib copyright (c) 2026 isaac
+    trimble-pederson methods and design taken from Game AI by Example by Mat
+    Buckland
+
+    ===
+
+    Copyright (c) 2026 Isaac Trimble-Pederson
+
+    This program is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along with
+    this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <cstdlib>
 
 #include <ankerl/unordered_dense.h>
--- src/states/GoalkeeperState.hpp
+++ src/states/GoalkeeperState.hpp
@@ -1,4 +1,25 @@
- /*
+/*
+    GoalkeeperState.hpp - Goalkeeper components relating to player state.
+
+    ===
+
+    Copyright (c) 2026 Isaac Trimble-Pederson
+
+    This program is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along with
+    this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
     Goalkeeper will _interpose_ between the ball and the goal line (rear),
     following a semicircle radius
 
